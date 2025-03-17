cd592f9a0819da651070cc48ff95d21007b44b14
6300539072 <vinodkumarpvk111@gmail.com>
2025-03-17 11:54:29 +0530

    sample

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+const port = process.env.PORT | 3004
+const serverless= require('serverless-http')
+
+app.get('/vinod',(req,res)=>{
+    res.status(200).send('i love my wife.She is very beatiful with character')
+})
+
+app.get('/checklistnaveen',(req,res)=>{
+    res.status(200).send('tested check list successfully running')
+})
+
+
+app.listen(port ,()=>{
+    console.log(`server running on http://localhost:${port}`)
+})
+// module.exports.handler = serverless(app)
